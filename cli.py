4f7a7b958398f068b60d14670d79ffcf4f007d76
Nicholas Finestead <naf3595@mavs.uta.edu>
2024-07-05 14:57:59 -0500

    Removed flag variables, and wrapped user input around try catch block to avoid IndexError exception

--- cli.py
+++ cli.py
@@ -3,57 +3,49 @@ from reorderPlaylist import SpotifyInterface, sort_dateadded, sort_alphabetic, s
 
 
 #   List of currently supported flags
-#   -p          Playlist Name
+#   -pn          Playlist Name
 #   -pu         Playlist URI
 #   -u          Spotify Username
-#   -d          Enable debug
+#   -d          Enable debug (FUTURE FLAG)
 #   -s          Sort Method Used
+
+
 def parse_flags(cmd_args: list) -> Union[dict, None]:
-    p_flag = False
-    pu_flag = False
-    u_flag = False
-    d_flag = False
+    
     flag_dict = {}
     flag_dict['Sort'] = sort_dateadded
     for i in range(1, len(cmd_args)):
-        #print(cmd_args[i])
-        if cmd_args[i] == "-p":
-            if not p_flag:
+        try:
+            if cmd_args[i] == "-pn":
                 flag_dict['Playlist_name'] = cmd_args[i+1]
                 i+=1
-                p_flag = True
-        elif cmd_args[i] == "-pu":
-            if not pu_flag:
+            elif cmd_args[i] == "-pu":
                 flag_dict['Playlist_uri'] = cmd_args[i+1]
                 i+=1
-                pu_flag = True
-        elif cmd_args[i] == "-u":
-            if not u_flag:
-                flag_dict['Username'] = cmd_args[i+1]
-        elif cmd_args[i] == "-s":
-                if cmd_args[i+1] == "Date":
-                    pass
-                if cmd_args[i+1] == "Alphabetic":
-                    flag_dict['Sort'] = sort_alphabetic
-                elif cmd_args[i+1] == "Artist":
-                    flag_dict['Sort'] = sort_artist
-                else:
+            elif cmd_args[i] == "-u":
+                    flag_dict['Username'] = cmd_args[i+1]
+            elif cmd_args[i] == "-s":
+                sorts = { "Date": sort_dateadded, "Alphabetic": sort_alphabetic, "Artist": sort_artist}
+                try:
+                    flag_dict['Sort'] = sorts[cmd_args[i+1]]
+                    i+=1
+                except:
                     print("ERROR: Unknown Sort option, defaulting to Date Added")
-        elif cmd_args[i] == "-d":
-            pass
-        elif cmd_args[i] == "-h":
-            print('''
+            elif cmd_args[i] == "-h":
+                help_string = '''
     To use this utility run the program by using "python main.py ..." 
     if no arguments are supplied the GUI will be opened, 
     or if incorrect arguments are supplied the program will exit
                 -u        Spotify Username
-                -p        Playlist Name, enclosed in quotes if multiple words
+                -pn        Playlist Name, enclosed in quotes if multiple words
                 -pu       Playlist Uri
-                -d        Debug Flag
                 -h        Help
-                -s        Sort Method(Date by default, Alphabetic, or Artist)
-''')
-            exit(0)
+                -s        Sort Method(Date added by default, Alphabetic, or Artist)'''
+                print(help_string)
+                exit(0)
+        except IndexError:
+                print(f"ERROR: \"{cmd_args[i]}\" Flag cannot be used without value")
+                exit(1)
     return flag_dict
     
     
